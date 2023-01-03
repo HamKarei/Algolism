199be410e487196dc9a4441faed5a04fdb1d3bfa
HamKarei <108329881+HamKarei@users.noreply.github.com>
2023-01-03 16:40:47 +0900

    Add files via upload

---
+++ 힙정렬.cpp
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+int number = 9;
+int heap[9] = { 7,6,5,8,3,5,9,1,6 };
+
+int main()
+{
+	for (int i = 1; i < 9; i++)
+	{
+		int c = i;
+		do {
+			int root = (c - 1) / 2; //root=부모 특정원소의 부모를 가르킴(소수점아래는 내림)
+			if (heap[root] < heap[c])
+			{
+				int temp = heap[root];
+				heap[root] = heap[c];
+				heap[c] = temp;
+			}
+			c = root;
+		} while (c != 0);
+	}
+	// 크기를 줄여가면서 반복적으로 힙을 구성
+	for (int i = 9 - 1; i >= 0; i--)
+	{
+		int temp = heap[0];
+		heap[0] = heap[i];
+		heap[i] = temp;
+		int root = 0;
+		int c = 1;
+		do
+		{
+			c = 2 * root + 1;
+			//자식 중에 더 큰 값을 찾기
+			if (heap[c] < heap[c + 1] && c < i - 1)
+			{
+				c++;
+			}
+			// 루트보다 자식이 더 크다면 교환
+			if (heap[root] < heap[c] && c < i)
+			{
+				int temp = heap[root];
+				heap[root] = heap[c];
+				heap[c] = temp;
+			}
+			root = c;
+		} while (c < i);
+	}
+
+	for (int i = 0; i < 9; i++)
+	{
+		printf("%d ", heap[i]);
+	}
+
+	return 0;
+}
